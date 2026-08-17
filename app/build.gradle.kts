302485b6346873c2474019958d241cc9c71fe329
Rajbyar1999913 <rajbyar09@gmail.com>
2026-08-17 19:21:19 +0530

    CHANGE CHUKARA AND ADD GAME WISE LIST

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.10"
+        versionCode = 12
+        versionName = "1.11"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
